0d6188de55177add528b6b107475d2fb6d99394a
devsultan06 <sultanabaniks@gamil.com>
2024-08-30 11:08:26 +0100

    created a folder for videos

--- src/Components/Movies Component/Video.jsx
+++ src/Components/Movies Component/Video.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import video from "/images/Avengers Endgame _ The Big Three Fight Scenes.mp4";
+import video from "../../../public/videos/Avengers Endgame _ The Big Three Fight Scenes.mp4";
 import Navbar from "../../utils/Navbar";
 import { useEffect } from "react";
 
