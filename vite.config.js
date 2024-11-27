7f6288d8157adf07f2ca6041eee82e7d117d53b5
Sultan A. Dev <sultanabaniks@gmail.com>
2024-11-27 19:32:41 +0100

    set base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // Make sure this is set correctly based on your deployment path
   plugins: [react()],
 });
