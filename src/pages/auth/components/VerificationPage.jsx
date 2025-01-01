62eaf2d212ac451834bc69ecd61c8a7a9ec7d627
Sultan A. Dev <sultanabaniks@gmail.com>
2025-01-01 16:58:18 +0100

    refactor verification page to improve user verification checks and error handling

--- src/pages/auth/components/VerificationPage.jsx
+++ src/pages/auth/components/VerificationPage.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { auth, firestore } from "../../../firebase/config/firebase";
 import { doc, updateDoc } from "firebase/firestore";
 import { ThreeDots } from "react-loader-spinner";
 
 const VerifiedPage = () => {
-  const navigate = useNavigate();
   const location = useLocation();
   const [isChecking, setIsChecking] = useState(true);
 
@@ -14,25 +13,37 @@ const VerifiedPage = () => {
     const verification = params.get("verification");
 
     if (verification !== "true") {
+      console.log("Verification query parameter missing or invalid.");
       window.location.href = "https://streamvibe06.vercel.app/auth";
       return;
     }
 
     const checkVerification = async () => {
       const intervalId = setInterval(async () => {
-        await auth.currentUser?.reload();
+        try {
+          if (auth.currentUser) {
+            await auth.currentUser.reload();
+            console.log("Reloading user...");
 
-        if (auth.currentUser && auth.currentUser.emailVerified) {
-          const userDocRef = doc(firestore, "users", auth.currentUser.uid);
-          await updateDoc(userDocRef, {
-            emailVerified: true,
-          });
+            if (auth.currentUser.emailVerified) {
+              console.log("User email is verified.");
+              // Update Firestore
+              const userDocRef = doc(firestore, "users", auth.currentUser.uid);
+              await updateDoc(userDocRef, {
+                emailVerified: true,
+              });
 
-          clearInterval(intervalId);
-          setIsChecking(false);
-          window.location.href = "https://streamvibe06.vercel.app/home";
-        } else if (auth.currentUser) {
-          console.log("Email not verified yet");
+              clearInterval(intervalId);
+              setIsChecking(false);
+              window.location.href = "https://streamvibe06.vercel.app/home";
+            } else {
+              console.log("Email not verified yet.");
+            }
+          } else {
+            console.log("No current user. Waiting for Firebase auth...");
+          }
+        } catch (error) {
+          console.error("Error while checking verification:", error);
         }
       }, 1000);
 
@@ -40,7 +51,7 @@ const VerifiedPage = () => {
     };
 
     checkVerification();
-  }, [navigate, location.search]);
+  }, [location.search]);
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black06">
