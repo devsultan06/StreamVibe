24b1833745ab544ca2e6e4ea93890872e53aa1e6
Sultan A. Dev <sultanabaniks@gmail.com>
2025-01-22 00:13:16 +0100

    refactor NotFound page styles; remove obsolete CSS rules and apply utility classes for improved layout and responsiveness

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -6,8 +6,8 @@ const NotFound = () => {
     document.title = "Page Not Found";
   });
   return (
-    <div className="not-found">
-      <div className="in">
+    <div className="not-found align-center inline-flex h-screen w-full justify-center bg-[#1a1a1a] text-center text-[30px] text-white">
+      <div className="in align-center flex justify-center gap-[1rem]">
         <img src={Image404} alt="" />
         <h1>404</h1>
         <h1>Page Not Found</h1>
