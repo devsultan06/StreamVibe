26f5aa41b37e63be46ff7fd1dcb6d3fad5372227
Sultan A. Dev <sultanabaniks@gmail.com>
2025-02-24 18:26:50 +0100

    feat: update image URLs in mock API database for external accessibility

--- mock-api/server.cjs
+++ mock-api/server.cjs
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const jsonServer = require('json-server');
 const path = require('path');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use('/images', express.static(path.join(__dirname, 'images')));
+
 server.use(middlewares);
 server.use(router);
 
@@ -14,5 +16,3 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`JSON Server is running on http://localhost:${port}`);
 });
-
-
