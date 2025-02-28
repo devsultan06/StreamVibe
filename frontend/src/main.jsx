bb76e29f2848d4211af7e8d6edebceb8d5a320d6
Sultan A. Dev <sultanabaniks@gmail.com>
2025-02-28 10:37:03 +0100

    introduction of a landing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./pages/landing-page/Landing.jsx";
 import Home from "./pages/home/Home.jsx";
 import Movies from "./pages/movies/Movies.jsx";
 import MovieShow from "./pages/movieShow/MovieShow.jsx";
@@ -31,38 +32,46 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       v7_startTransition: true,
     }}
   >
-    {" "}
-    <AuthProvider>
-      {isUnderDevelopment ? (
-        <MaintenancePage />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Navigate to="/auth" replace />} />
-          <Route path="/auth" element={<Authentication />} />
-          <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-          <Route
-            path="/movies"
-            element={<ProtectedRoute element={<Movies />} />}
-          />
-          <Route
-            path="/movieshow"
-            element={<ProtectedRoute element={<MovieShow />} />}
-          />
-          <Route
-            path="/subscription"
-            element={<ProtectedRoute element={<Subscription />} />}
-          />
-          <Route path="/support" element={<Support />} />{" "}
-          {/* No authentication required for support */}
-          <Route
-            path="/profile"
-            element={<ProtectedRoute element={<Profile />} />}
-          />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/verified" element={<VerificationPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      )}
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route
+        path="/*"
+        element={
+          <AuthProvider>
+            {isUnderDevelopment ? (
+              <MaintenancePage />
+            ) : (
+              <Routes>
+                <Route path="/auth" element={<Authentication />} />
+                <Route
+                  path="/home"
+                  element={<ProtectedRoute element={<Home />} />}
+                />
+                <Route
+                  path="/movies"
+                  element={<ProtectedRoute element={<Movies />} />}
+                />
+                <Route
+                  path="/movieshow"
+                  element={<ProtectedRoute element={<MovieShow />} />}
+                />
+                <Route
+                  path="/subscription"
+                  element={<ProtectedRoute element={<Subscription />} />}
+                />
+                <Route path="/support" element={<Support />} />{" "}
+                <Route
+                  path="/profile"
+                  element={<ProtectedRoute element={<Profile />} />}
+                />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/verified" element={<VerificationPage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            )}
+          </AuthProvider>
+        }
+      />
+    </Routes>
   </BrowserRouter>,
 );
---
+++ frontend/src/pages/landing-page/Landing.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Logo from "/images/Logo.png";
+import { motion } from "framer-motion";
+import { NavLink, useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="relative flex h-screen flex-col items-center justify-center bg-cover bg-center text-white"
+      style={{
+        backgroundImage: 'url("/images/StreamVibe-Home-Background.png")',
+      }}
+    >
+      <div className="bg-black absolute left-0 top-0 mt-[15px] flex w-full items-center justify-between bg-opacity-50 p-[1.5rem] px-[5rem] max-1075:p-[10px]">
+        <NavLink to="/">
+          <img src={Logo} alt="StreamVibe Logo" className="max-800:w-[170px]" />
+        </NavLink>
+        <button
+          className="text-lg rounded-md bg-red45 px-6 py-2 font-semibold hover:bg-black10"
+          onClick={() => {
+            navigate("/auth");
+          }}
+        >
+          Register
+        </button>
+      </div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="text-center"
+      >
+        <motion.h1
+          className="md:text-8xl text-[50px] font-extrabold uppercase"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.3 }}
+        >
+          <span className="bg-gradient-to-r from-red-500 to-yellow-400 text-transparent block bg-clip-text">
+            WELCOME TO Stream
+          </span>
+          <motion.span
+            className="bg-gradient-to-r from-yellow-400 to-red-500 text-transparent block bg-clip-text"
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.2, delay: 0.8 }}
+          >
+            Vibe
+          </motion.span>
+        </motion.h1>
+        <motion.p
+          className="md:text-2xl text-gray-300 mt-4 text-[20px]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          Your ultimate destination for streaming the latest movies and TV
+          shows.
+        </motion.p>
+        <button
+          className="text-lg mt-[20px] rounded-md bg-red45 px-6 py-2 font-semibold hover:bg-black10"
+          onClick={() => {
+            navigate("/auth");
+          }}
+        >
+          Get Started
+        </button>
+      </motion.div>
+    </div>
+  );
+};
+
+export default LandingPage;
